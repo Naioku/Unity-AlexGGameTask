9f69445dea90b57bcf45433b45daae553ed82726
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-10-29 23:11:43 +0200

    Drag&Drop - part 1.

--- AlexGGameTask/Assets/Scripts/DragAndDropController.cs
+++ AlexGGameTask/Assets/Scripts/DragAndDropController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class DragAndDropController : MonoBehaviour
 {
+    [SerializeField] private float maxRaycastDistance = 40f;
+    [SerializeField] private LayerMask searchingLayers;
+    
     private InputReader _inputReader;
     private GameObject _selectedObject;
 
@@ -15,9 +18,62 @@ public class DragAndDropController : MonoBehaviour
     {
         _inputReader.SelectionActivatedEvent += HandleClick;
     }
+    
+    private void OnDisable()
+    {
+        _inputReader.SelectionActivatedEvent -= HandleClick;
+    }
+
+    private void Update()
+    {
+        if (_selectedObject)
+        {
+            ManageDragging();
+        }
+    }
+
+    private void ManageDragging()
+    {
+        print("Managing dragging...");
+    }
 
     private void HandleClick(bool isPressed)
     {
-        print(isPressed ? "Pressed." : "Released.");
+        if (isPressed)
+        {
+            PickUp();
+        }
+        else
+        {
+            PutDown();
+        }
+    }
+
+    private void PickUp()
+    {
+        _selectedObject = GetClickedObject();
+        print("Picking up...");
+    }
+
+    private void PutDown()
+    {
+        _selectedObject = null;
+        print("Putting down...");
+    }
+
+    private GameObject GetClickedObject()
+    {
+        bool hasHit = Physics.Raycast(
+            GetMouseRay(_inputReader.MouseScreenPosition),
+            out RaycastHit hit,
+            maxRaycastDistance,
+            searchingLayers);
+            
+        return hasHit ? hit.collider.gameObject : null;
+    }
+    
+    private static Ray GetMouseRay(Vector2 mousePosition)
+    {
+        return Camera.main.ScreenPointToRay(mousePosition);
     }
 }
