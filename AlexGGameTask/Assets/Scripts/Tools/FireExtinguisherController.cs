ca19c564021ceb75e571e1bbb83a3d6f752666fd
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-10-30 18:01:41 +0100

    ExtinguishingController fully set up.

--- AlexGGameTask/Assets/Scripts/Tools/FireExtinguisherController.cs
+++ AlexGGameTask/Assets/Scripts/Tools/FireExtinguisherController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace Tools
@@ -6,15 +8,66 @@ namespace Tools
     {
         [SerializeField] private float minHeight = 1;
         [SerializeField] private float maxHeight = 4;
-
         [Range(0, 1)]
-        [SerializeField] private float heightLevel = 0.5f;
+        [SerializeField] private float powderLevel = 1;
+        [SerializeField] private TextMeshProUGUI powderLevelLabel;
+        [Tooltip("On how long should last when filled to max value? [seconds]")]
+        [SerializeField] private float durationWhenMax = 10;
+        [SerializeField] private ParticleSystem particles;
 
-        private void OnValidate()
+        private float _remainingTimeOfUse;
+        private bool _isWorking;
+        private Coroutine _stifleCoroutine;
+
+        private bool ShouldWorking => _remainingTimeOfUse > 0;
+
+        private void Start()
+        {
+            _remainingTimeOfUse = powderLevel * durationWhenMax;
+            DisplayPowderLevel();
+        }
+
+        public void SetHeightLevel(float value)
         {
             Vector3 toolPosition = transform.position;
-            toolPosition.y = Mathf.Lerp(minHeight, maxHeight, heightLevel);
+            toolPosition.y = Mathf.Lerp(minHeight, maxHeight, value);
             transform.position = toolPosition;
+        }
+
+        public void StartStifle()
+        {
+            if (!ShouldWorking) return;
+            particles.Play();
+
+            if (_stifleCoroutine != null)
+            {
+                StopCoroutine(_stifleCoroutine);
+                particles.Stop();
+            }
+            _stifleCoroutine = StartCoroutine(StifleCoroutine());
+        }
+        
+        public void StopStifle()
+        {
+            if (!ShouldWorking) return;
+            particles.Stop();
+        }
+
+        private IEnumerator StifleCoroutine()
+        {
+            while (ShouldWorking)
+            {
+                _remainingTimeOfUse = Mathf.Max(0, _remainingTimeOfUse - Time.deltaTime);
+                powderLevel = _remainingTimeOfUse / durationWhenMax;
+                DisplayPowderLevel();
+                yield return null;
+            }
+            particles.Stop();
+        }
+
+        private void DisplayPowderLevel()
+        {
+            powderLevelLabel.text = Mathf.Round(powderLevel * 100).ToString() + "%";
         }
     }
 }
---
+++ AlexGGameTask/Assets/Scripts/Utils/OnHoldButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Utils
+{
+    public class OnHoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private UnityEvent ButtonDown;
+        [SerializeField] private UnityEvent ButtonUp;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            ButtonDown.Invoke();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            ButtonUp.Invoke();
+        }
+    }
+}
